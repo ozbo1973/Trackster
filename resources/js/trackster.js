db27b770a01936db9ad26727bc012d583897eccd
ozbo1973 <bbbove20@gmail.com>
2017-04-20 06:40:22 -0600

    Adding script to pull data from api.

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,8 +1,20 @@
+// Document ready function
+$(document).ready(function() {
+  $('#search-button').click(function() {
+    /* Act on the event */
+    var searchInput = $('#search-input').val();
+    Trackster.searchTracksByTitle(searchInput);
+  });// ./click
+
+
+});
+
+
 var Trackster = {};
 
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
-  Append each "row" to the container in the body to display all tracks. 
+  Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
 
@@ -13,5 +25,18 @@ Trackster.renderTracks = function(tracks) {
   Render the tracks given in the API query response.
 */
 Trackster.searchTracksByTitle = function(title) {
+  //call to the API
+  $.ajax({
+    url: 'https://api.spotify.com/v1/search?type=track&q='+ title,
+    type: 'GET',
+    dataType: 'json',
+  })
+  .done(function(data) {
+    console.log("success");
+    console.log(data.tracks.items);
+  })
+  .fail(function() {
+    console.log("error");
+  })
 
 };
