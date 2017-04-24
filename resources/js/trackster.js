2fed77ea62bfe0647c1177d25f6b4a194b92a6b7
ozbo1973 <bbbove20@gmail.com>
2017-04-24 07:09:04 -0600

    Completing Js file to display tracks.

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -2,9 +2,23 @@
 $(document).ready(function() {
   $('#search-button').click(function() {
     /* Act on the event */
+    $('.header h1').css('color', 'whitesmoke');
+
     var searchInput = $('#search-input').val();
     Trackster.searchTracksByTitle(searchInput);
+
+    //change title logo back to pink
+    $('.header h1').css('color', 'rgb(255, 0, 171)');
+
   });// ./click
+
+  $('#search-input').keypress(function(event) {
+    /* Act on the event */
+    if(event.which === 13){
+      event.preventDefault();
+      $('#search-button').click();
+    }
+  }); // ./keypress
 
 
 });
@@ -17,7 +31,27 @@ var Trackster = {};
   Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
+  var $trackList = $('#display-tracks');
 
+  $trackList.empty();
+
+  $.each(tracks,function(i, v) {
+    var track=tracks[i];
+    var convertTrackLength= (track.duration_ms / 1000) / 60;
+
+  //template of rows for tracks
+    var trackHTML=
+      '<div class="row track">' +
+      '<span class="col-xs-1 play-btn"><a href="' + track.preview_url + '"><i class="fa fa-play-circle-o fa-2x"></i></a>' +
+      '</span><span class="col-xs-1 text-right">'+ track.track_number + '</span>' +
+      '<span class="col-xs-3">' + track.name + '</span>' +
+      '<span class="col-xs-2">' + track.artists[0].name + '</span>' +
+      '<span class="col-xs-3">' + track.album.name + '</span>' +
+      '<span class="col-xs-1">' + track.popularity + '</span>' +
+      '<span class="col-xs-1">' + convertTrackLength.toFixed(2).replace('.',':') + '</span></div>';
+  //add to div
+      $('#display-tracks').append(trackHTML);
+  });
 };
 
 /*
@@ -30,13 +64,15 @@ Trackster.searchTracksByTitle = function(title) {
     url: 'https://api.spotify.com/v1/search?type=track&q='+ title,
     type: 'GET',
     dataType: 'json',
+    success: function(response){
+      Trackster.renderTracks(response.tracks.items);
+    }
   })
-  .done(function(data) {
-    console.log("success");
-    console.log(data.tracks.items);
-  })
-  .fail(function() {
-    console.log("error");
-  })
+  // .done(function() {
+  //   console.log("success");
+  // })
+  // .fail(function() {
+  //   console.log("error");
+  // })
 
 };
