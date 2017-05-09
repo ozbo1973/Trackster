579e00eee1b34bedf8ee9ed1b070b70c534aee43
ozbo1973 <bbbove20@gmail.com>
2017-05-09 07:14:37 -0600

    Adding function to sort by header.

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     $('.header h1').css('color', 'whitesmoke');
 
     var searchInput = $('#search-input').val();
-    Trackster.searchTracksByTitle(searchInput);
+    Trackster.searchTracksByTitle(searchInput,'none');
 
     //change title logo back to pink
     $('.header h1').css('color', 'rgb(255, 0, 171)');
@@ -20,8 +20,17 @@ $(document).ready(function() {
     }
   }); // ./keypress
 
+  $('.header-attribute').click(function(event) {
+    /* Act on the event */
+    var sel=$(this).text();
+    var searchInput = $('#search-input').val();
+    Trackster.searchTracksByTitle(searchInput,sel);
+  });
+  // end click
+
 
 });
+// ./doc ready
 
 
 var Trackster = {};
@@ -32,47 +41,79 @@ var Trackster = {};
 */
 Trackster.renderTracks = function(tracks) {
   var $trackList = $('#display-tracks');
-
   $trackList.empty();
 
   $.each(tracks,function(i, v) {
     var track=tracks[i];
     var convertTrackLength= (track.duration_ms / 1000) / 60;
 
+    // console.log(track.name[0]);
   //template of rows for tracks
     var trackHTML=
       '<div class="row track">' +
       '<span class="col-xs-1 play-btn"><a href="' + track.preview_url + '"><i class="fa fa-play-circle-o fa-2x"></i></a>' +
       '</span><span class="col-xs-1 text-right">'+ track.track_number + '</span>' +
-      '<span class="col-xs-3">' + track.name + '</span>' +
+      '<span class="col-xs-3 track-name" >' + track.name + '</span>' +
       '<span class="col-xs-2">' + track.artists[0].name + '</span>' +
       '<span class="col-xs-3">' + track.album.name + '</span>' +
       '<span class="col-xs-1">' + track.popularity + '</span>' +
       '<span class="col-xs-1">' + convertTrackLength.toFixed(2).replace('.',':') + '</span></div>';
+
+  //add data elements
+      // $('.track-name').data('trackName', track.name[0]);
+
+
   //add to div
       $('#display-tracks').append(trackHTML);
-  });
+
+  }); // ./each
+
 };
 
 /*
   Given a search term as a string, query the Spotify API.
   Render the tracks given in the API query response.
 */
-Trackster.searchTracksByTitle = function(title) {
+Trackster.searchTracksByTitle = function(title, header) {
   //call to the API
   $.ajax({
     url: 'https://api.spotify.com/v1/search?type=track&q='+ title,
     type: 'GET',
     dataType: 'json',
     success: function(response){
-      Trackster.renderTracks(response.tracks.items);
-    }
-  })
-  // .done(function() {
-  //   console.log("success");
-  // })
-  // .fail(function() {
-  //   console.log("error");
-  // })
+      var list = response.tracks.items;
+      //change header based on header clicked
+      if(header !== 'none') {
+          switch (header) {
+            case 'Song':
+              header = 'name';
+              break;
+            case 'Album':
+              header = 'album';
+              break;
+            case 'Popularity':
+              header= 'popularity'
+              break;
+            case 'Length':
+              header= 'duration_ms'
+              break;
+            default:
+              header = 'none'
+              break;
+          } // ./switch
 
+          list= Trackster.sortList(list,header);
+      } // ./if
+      Trackster.renderTracks(list);
+    } //./success
+  })
 };
+
+Trackster.sortList=function(tracks,key){
+  return tracks.sort(function(a,b){
+    var x=a[key]; var y=b[key];
+    if(key === 'album') {x=a[key]["name"]; y=b[key]["name"]};
+    console.log(y);
+    return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+  })
+}
